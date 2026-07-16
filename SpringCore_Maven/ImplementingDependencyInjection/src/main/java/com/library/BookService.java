89e60df6ecb9ca8ed88ee71e8f37d4bc35ca65f9
Trishita01 <trishitapodder3@gmail.com>
2026-07-16 12:28:10 +0530

    Ex:1 Added Implementing Dependency Injection

---
+++ SpringCore_Maven/ImplementingDependencyInjection/src/main/java/com/library/BookService.java
@@ -0,0 +1,18 @@
+package com.library;
+
+public class BookService {
+
+    private BookRepository bookRepository;
+
+    // Setter method for Dependency Injection
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void addBook() {
+
+        System.out.println("Adding book using BookService");
+
+        bookRepository.saveBook();
+    }
+}
