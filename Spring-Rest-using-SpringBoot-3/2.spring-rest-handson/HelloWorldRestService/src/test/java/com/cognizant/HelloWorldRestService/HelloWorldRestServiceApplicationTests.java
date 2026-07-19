0dfa008dcd123744d22563f0ed198c44db96618e
Trishita01 <trishitapodder3@gmail.com>
2026-07-19 10:38:46 +0530

    Completed 2,Spring rest handson Hello world Rest service

---
+++ Spring-Rest-using-SpringBoot-3/2.spring-rest-handson/HelloWorldRestService/src/test/java/com/cognizant/HelloWorldRestService/HelloWorldRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.HelloWorldRestService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
