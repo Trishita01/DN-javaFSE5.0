745344f174907a130e1a5be8d976fc6697ee57c1
Trishita01 <trishitapodder3@gmail.com>
2026-07-19 11:09:21 +0530

    added 2. spring rest handson REST country web service

---
+++ Spring-Rest-using-SpringBoot-3/2.spring-rest-handson/RESTCountrywebservice/src/test/java/com/cognizant/RESTCountrywebservice/RestCountrywebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.RESTCountrywebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestCountrywebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
