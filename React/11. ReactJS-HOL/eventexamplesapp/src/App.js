9247460317e57fc629aea106324e58e6efd6b8de
Trishita01 <trishitapodder3@gmail.com>
2026-07-23 23:25:49 +0530

    Added 11.ReactJS-HOL

---
+++ React/11. ReactJS-HOL/eventexamplesapp/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+import EventExamples from './Components/EventExamples';
+import CurrencyConvertor from './Components/CurrencyConvertor';
+
+function App() {
+
+  return (
+
+    <div>
+
+      <EventExamples />
+
+      <hr />
+
+      <CurrencyConvertor />
+
+    </div>
+
+  );
+
+}
+
+export default App;
